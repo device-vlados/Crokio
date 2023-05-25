75a31a36147a85ff777b8dd956474274a1df3e39
device-vlados <102617814+device-vlados@users.noreply.github.com>
2023-05-25 17:41:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-$(function () {
+const circles = document.querySelectorAll(".circle"),
+    progressBar = document.querySelector(".indicator"),
+    buttons = document.querySelectorAll("button");
+let currentStep = 1;
+const updateSteps = (e) => {
+    currentStep = e.target.id === "next" ? ++currentStep : --currentStep;
+    circles.forEach((circle, index) => {
+        circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
+    });
+    progressBar.style.width = `${((currentStep - 1) / (circles.length - 1)) * 100}%`;
+    if (currentStep === circles.length) {
+        buttons[1].disabled = true;
+    } else if (currentStep === 1) {
+    buttons[0].disabled = true;
+    } else {
+    buttons.forEach((button) => (button.disabled = false));
+    }
+};
+buttons.forEach((button) => {
+    button.addEventListener("click", updateSteps);
+});
 
+$(function () {
+// -----------------------------------------------------------------------------
     $('.testimonials__slider').slick({
         slidesToShow: 1,
         arrows: false,
@@ -15,13 +37,22 @@ $(function () {
         e.preventDefault()
         $('.testimonials__slider').slick('slickNext')
     })
-
+// -----------------------------------------------------------------------------
     $('.case__slider').slick({
         slidesToShow: 2,
         arrows: false,
         dots: false,
         waitForAnimate: true,
         infinite: true,
+        responsive: 
+        [
+            {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 1,
+            },
+            },
+        ]
     })
     $('.case__slider-prev').on('click', function (e) {
         e.preventDefault()
@@ -31,5 +62,9 @@ $(function () {
         e.preventDefault()
         $('.case__slider').slick('slickNext')
     })
-
+// -----------------------------------------------------------------------------
+    $('.burger').on('click', function (e) {
+        e.preventDefault()
+        $('.header__nav').toggleClass('header__nav--open')
+    })
 })
